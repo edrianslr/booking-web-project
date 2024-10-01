4b7818ac5b70e211d28b9d36f573146b1ba8868f
Edrian <91896365+edrianslr@users.noreply.github.com>
2024-10-01 21:35:54 +0800

    Initial commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import "./home.css"
 import Data from './Data'
+import {motion} from 'framer-motion'
+import { fadeIn } from '../../variants'
 
 const Home = () => {
     return (
         <section className="home section" id="home">
             <div className="home_container container grid">
-                <div className="home_content grid">
+                <motion.div 
+                variants={fadeIn("up", 0.2)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.7 }}
+                
+                className="home_content grid">
                     <div className="home_img"></div>
     
                     <Data />
-                </div>
+                </motion.div>
             </div>
         </section>
       )
